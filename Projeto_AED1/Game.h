c7b569eb4c72e8aabcefd1e6fb63075555bc1185
Matheus de Moura Rosa <matedits03216@gmail.com>
2021-05-22 21:22:51 -0300

    Começando toda a lógica do zero.

--- Projeto_AED1/Game.h
+++ Projeto_AED1/Game.h
@@ -15,50 +15,10 @@ using namespace std;
 */
 class Game
 {
+
 private:
-	//Variáveis
-	//Janela
-	sf::RenderWindow* window;
-	sf::VideoMode videoMode;
-	sf::Event eventPlayer;
 
-	//Posicoes do Mouse
-
-	sf::Vector2i mousePosWindow;
-
-	//Game logic
-	int points;
-	float enemySpawnTimer;
-	float enemySpawnTimerMax;
-	int maxEnemies;
-
-	//Game Objects
-
-	sf::RectangleShape enemy;
-	vector <sf::RectangleShape> enemies;
-
-	//Funcões Privadas.
-	void initVariables();
-	void initWindow();
-	void initEnemies();
 public:
 
-	//Construtores e Destruidores 
-
-	Game();
-	virtual ~Game();
-
-	//Acessores
-
-	const bool running() const;
-
-	//Funçoes
-	void spawnEnemies(); //Inimigos
-	void pollEvents();
-	void updateMousePositions();
-	void updateEnemies(); //Inimigos
-	void update();
-	void renderEnemies(); //Inimigos
-	void render();
 };
 
--- Projeto_AED1/main.cpp
+++ Projeto_AED1/main.cpp
@@ -3,23 +3,6 @@
 // Principal
 int main()
 {
-	//Inicializar o srand
-
-	srand(static_cast<unsigned>(time(NULL)));
-
-	//Inicializar o jogo
-	Game game;
-
-	//Game Loop
-	while (game.running()) 
-	{
-		//Update
-		game.update();
-
-		//Render
-		game.render();
-		
-	}
 
 	//Fim do aplicativo
 	return 0;
