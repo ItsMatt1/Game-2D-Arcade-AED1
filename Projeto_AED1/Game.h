f1453ef5162ffdd3533236fedd71cd38fe7466f4
Matheus de Moura Rosa <matedits03216@gmail.com>
2021-05-23 02:56:10 -0300

    Atualização do main.cpp

--- Projeto_AED1/Game.h
+++ Projeto_AED1/Game.h
@@ -15,8 +15,6 @@ using namespace std;
 */
 class Game
 {
-		
-	
 
 private:
 
--- Projeto_AED1/main.cpp
+++ Projeto_AED1/main.cpp
@@ -3,11 +3,19 @@
 // Principal
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Projeto AED1");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Projeto AED1");
 	window.setFramerateLimit(60);
 
+
 	sf::Texture t1;
-	//t1.loadFromFile("image/spaceship.png");
+	t1.loadFromFile("Assets/Asteroid.png");
+
+	sf::Sprite Asteroid(t1);
+
+	Asteroid.setPosition(sf::Vector2f(320.f, 220.f));
+
+
+	//t1.setSmooth(false);
 
 	while (window.isOpen())
 	{
@@ -27,15 +35,12 @@ int main()
 		window.clear(sf::Color::Black);
 
 		//Desenhar
+		window.draw(Asteroid);
 
 		//Terminar o Frame
 		window.display();
 
 	}
-
-	
-
-
 	//Desenhar
 
 	//window.clear();
